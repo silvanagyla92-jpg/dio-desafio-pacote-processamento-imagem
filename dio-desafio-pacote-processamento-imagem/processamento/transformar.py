27b5795580ec6d715dcf152aa7ef2cb64fb2a211
Nágyla Silva <silvanagyla92@gmail.com>
2026-08-09 19:47:38 -0300

    Refactor image resizing for better readability

--- dio-desafio-pacote-processamento-imagem/processamento/transformar.py
+++ dio-desafio-pacote-processamento-imagem/processamento/transformar.py
@@ -1,8 +1,13 @@
 from skimage.transform import resize
 
+
 def redimensionar_imagem(image, proporcao):
     assert 0 <= proporcao <= 1, "Informe uma proporção entre 0 e 1!"
     height = round(image.shape[0] * proporcao)
     width = round(image.shape[1] * proporcao)
-    imagem_redimensionada = resize(imagem, (height, width), anti_aliasing=True)
+    imagem_redimensionada = resize(
+        image,
+        (height, width),
+        anti_aliasing=True
+    )
     return imagem_redimensionada
